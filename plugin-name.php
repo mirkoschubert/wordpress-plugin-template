e01f6a46dc27b04acaea48132dd02f4e93a336e1
mirkoschubert <office@mirkoschubert.de>
2023-05-29 13:04:38 +0200

    Initial commit

---
+++ plugin-name.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Plugin Name:       WordPress Plugin Template
+ * Plugin URI:        https://example.com/plugin-name-uri/
+ * Description:       A opinionated WordPress Plugin Template.
+ * Author:            Mirko Schubert
+ * Author URI:        https://mirkoschubert.de/
+ * 
+ * Version:           1.0.0
+ * Requires at least: 5.6
+ * Tested up to:      6.3
+ * 
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * 
+ * Text Domain:       plugin-name
+ * Domain Path:       /languages
+ * 
+ * @package           Plugin_Name
+ * @author            Mirko Schubert
+ * @link              https://mirkoschubert.de/
+ * @since             1.0.0
+ */
+
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+define('PLUGIN_VERSION', '1.0.0');
+
